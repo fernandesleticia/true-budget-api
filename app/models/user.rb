1a4d63222761212dc09b636ea0177908a14b57fb
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-08-08 18:11:09 -0300

    use has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
+
+  has_secure_password
 end
