1643efbc91d78f997c84549230d67cc98e075793
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-08-13 16:26:19 -0300

    finish group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,13 +2,25 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @groups = Group.all
+    render json: @groups 
   end
 
   def show
+    @group = Group.find(params[:id])
+    render json: @group
   end
 
   def create
-    @group = Group.new(params)
+    @group = Group.new(group_params)
     @group.save
+
+    render json: @group
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
