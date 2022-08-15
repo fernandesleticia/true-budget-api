8e0f5cabc573c46f5086a06ff95c31ba112e310f
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-08-15 19:45:30 -0300

    create user preferences model

---
+++ db/migrate/20220815224443_create_user_preferences.rb
@@ -0,0 +1,9 @@
+class CreateUserPreferences < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_preferences do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_215151) do
+ActiveRecord::Schema.define(version: 2022_08_15_224443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,13 @@ ActiveRecord::Schema.define(version: 2022_08_15_215151) do
     t.index ["category_id"], name: "index_transactions_on_category_id"
   end
 
+  create_table "user_preferences", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_preferences_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -81,4 +88,5 @@ ActiveRecord::Schema.define(version: 2022_08_15_215151) do
   add_foreign_key "category_budgets", "categories"
   add_foreign_key "transactions", "categories"
   add_foreign_key "transactions", "users"
+  add_foreign_key "user_preferences", "users"
 end
