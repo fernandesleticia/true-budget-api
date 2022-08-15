acd715f8a690380e5cdaeded6147a3e9cdb84fd8
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-08-15 19:46:38 -0300

    create wallet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_224443) do
+ActiveRecord::Schema.define(version: 2022_08_15_224603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,9 +84,18 @@ ActiveRecord::Schema.define(version: 2022_08_15_224443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wallets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.float "balance"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
   add_foreign_key "categories", "groups"
   add_foreign_key "category_budgets", "categories"
   add_foreign_key "transactions", "categories"
   add_foreign_key "transactions", "users"
   add_foreign_key "user_preferences", "users"
+  add_foreign_key "wallets", "users"
 end
