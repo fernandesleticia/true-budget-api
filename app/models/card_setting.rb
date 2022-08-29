329501cf8eaefcb6b7376f93cf97de7995b152ee
Leticia Fernandes <leticiafernandes777@gmail.com>
2022-08-29 19:36:51 -0300

    Adds card model

--- app/models/card_setting.rb
+++ app/models/card_setting.rb
@@ -1,3 +1,4 @@
 class CardSetting < ApplicationRecord
   belongs_to :user_preference
+  has_many :cards
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_224603) do
+ActiveRecord::Schema.define(version: 2022_08_29_223245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "card_settings", force: :cascade do |t|
+    t.bigint "user_preference_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_preference_id"], name: "index_card_settings_on_user_preference_id"
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -92,6 +106,7 @@ ActiveRecord::Schema.define(version: 2022_08_15_224603) do
     t.index ["user_id"], name: "index_wallets_on_user_id"
   end
 
+  add_foreign_key "card_settings", "user_preferences"
   add_foreign_key "categories", "groups"
   add_foreign_key "category_budgets", "categories"
   add_foreign_key "transactions", "categories"
