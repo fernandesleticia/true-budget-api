46fc053e89167915d6ac07e46b6420dbc23119bc
Letícia Fernandes <leticiafernandes777@gmail.com>
2022-09-20 20:03:48 -0300

    Preparing App for docker (#17)
    * add missing gems
    
    * Update application configuration
    
    * creating unicorn config
    
    * adding sidekiq initialize config
    
    * Whitelist Docker Host
    
    Rails have a security feature that blocks access from unknown sources.
    We want our different docker containers to communicate with each other,
    so we need to whitelist the container.
    
    * add env variables

--- config/application.rb
+++ config/application.rb
@@ -21,20 +21,15 @@ Bundler.require(*Rails.groups)
 
 module TrueBudgetApi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.cache_store = :redis_store, ENV['CACHE_URL'],
+      { namespace: 'true_budget_api::cache' }
+
+    config.active_job.queue_adapter = :sidekiq
     config.api_only = true
   end
 end
